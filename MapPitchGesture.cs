18577e3b7e09464157d06298295b53945c2515ec
Jan Rabe <janrabe@kibotu.net>
2015-04-01 12:51:30 +0200

    updated gestures in order to use unregistser handler and to own only one map, also removed argument exception for touch points

--- MapPitchGesture.cs
+++ MapPitchGesture.cs
@@ -1,50 +1,45 @@
-﻿using Microsoft.Phone.Maps.Controls;
-using System;
+﻿using System;
 using System.Windows.Input;
 
-namespace MultiTouchMap
+namespace JaguarRoutePlanner.Sources.Utils.Gestures
 {
-  /// <summary>
-  /// Adds a three-finger pitch gesture to a Map control.
-  /// </summary>
-  public class MapPitchGesture : MapGestureBase
-  {
     /// <summary>
-    /// Gets or sets the sensitivity of this gesture
+    ///     Adds a three-finger pitch gesture to a Map control.
     /// </summary>
-    public double Sensitivity { get; set; }
-
-    private double? _initialPitchYLocation;
-
-    public MapPitchGesture(Map map)
-      : base(map)
+    public class MapPitchGesture : MapGestureBase
     {
-      Sensitivity = 0.5;
-      Touch.FrameReported += Touch_FrameReported;
-    }
+        private double? _initialPitchYLocation;
 
-    private void Touch_FrameReported(object sender, TouchFrameEventArgs e)
-    {
-      var touchPoints = e.GetTouchPoints(Map);
-
-      SuppressMapGestures = touchPoints.Count == 3;
-
-      if (touchPoints.Count == 3)
-      {
-        if (!_initialPitchYLocation.HasValue)
+        public MapPitchGesture()
         {
-          _initialPitchYLocation = touchPoints[0].Position.Y;
+            Sensitivity = 0.5;
         }
 
-        double delta = touchPoints[0].Position.Y - _initialPitchYLocation.Value;
-        double newPitch = Math.Max(0, Math.Min(75, (Map.Pitch + delta * Sensitivity)));
-        Map.Pitch = newPitch;
-        _initialPitchYLocation = touchPoints[0].Position.Y;
-      }
-      else
-      {
-        _initialPitchYLocation = null;
-      }
+        /// <summary>
+        ///     Gets or sets the sensitivity of this gesture
+        /// </summary>
+        public double Sensitivity { get; set; }
+
+        protected override void OnTouch(TouchPointCollection touchPoints)
+        {
+            SuppressMapGestures = touchPoints.Count == 3;
+
+            if (touchPoints.Count == 3)
+            {
+                if (!_initialPitchYLocation.HasValue)
+                {
+                    _initialPitchYLocation = touchPoints[0].Position.Y;
+                }
+
+                var delta = touchPoints[0].Position.Y - _initialPitchYLocation.Value;
+                var newPitch = Math.Max(0, Math.Min(75, (Map.Pitch + delta*Sensitivity)));
+                Map.Pitch = newPitch;
+                _initialPitchYLocation = touchPoints[0].Position.Y;
+            }
+            else
+            {
+                _initialPitchYLocation = null;
+            }
+        }
     }
-  }
 }
